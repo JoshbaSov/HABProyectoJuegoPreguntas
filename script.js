f7f33c987cf07a82176f68ca951639a3d6b0eaaf
Mauro Oliver <olivermauro@outlook.com>
2024-08-09 00:17:55 +0200

    Correccion de bug de 50 preguntas

--- script.js
+++ script.js
@@ -1,140 +1,159 @@
 "use strict";
 let preguntasJson;
 
+fetch("quiz.json")
+  .then(function (respuesta) {
+    return respuesta.json();
+  })
+  .then(function (obj) {
+    //console.log(obj);
+    preguntasJson = obj;
 
-fetch("quiz.json").then(function (respuesta) {
-  return respuesta.json();
+    let numeroPreguntas;
+    do {
+      numeroPreguntas = parseInt(
+        prompt(
+          "Inserte cuantas preguntas entre 1 y 50. PISTA: cuantas mas insertes mas opciones de ganar"
+        )
+      );
+    } while (isNaN(numeroPreguntas) || numeroPreguntas < 1 || numeroPreguntas > 50);
+    let contador = 0;
+    let aciertos = 0;
+    const h2 = document.querySelector("h2");
 
-}).then(function (obj) {
-  //console.log(obj);
-  preguntasJson = obj;
+    //const respuesta = document.querySelector("span");
 
-  let numeroPreguntas;
-  do {
-    numeroPreguntas = parseInt(prompt("inserte cuantas preguntas entre 1 y 50. PISTA: cuantas mas insertes mas opciones de ganar"));
-  } while (isNaN(numeroPreguntas) || numeroPreguntas < 1 || numeroPreguntas >= 50);
-  let contador = 0;
-  let aciertos = 0;
-  const h2 = document.querySelector("h2");
-
-  //const respuesta = document.querySelector("span");
-
-  function recorrerLi(opciones) {
-    const ul = document.getElementById("lista").getElementsByTagName("li");
-    for (let i = 0; i < opciones.length; i++) {
-      const li = ul[i];
-      const span =
-        li.querySelector(
-          "span"
-        ); /* Se ingresa la respuesta en SPAN para no sobreescribir el INPUT RADIO */
-      span.textContent = opciones[i];
-    }
-  }
-
-
-  function hacerPregunta() {
-    let objetoEnpantalla = Object.values(preguntasJson[contador]);
-    //console.log(objetoEnpantalla[0]);
-    h2.innerHTML = `${objetoEnpantalla[0]}`;
-
-    //console.log(objetoEnpantalla[1]);
-    let opciones = objetoEnpantalla[1];
-    recorrerLi(opciones);
-  }
-
-  const botonEnviar = document.querySelector("#btnEnviar");
-  const botonRecargar = document.querySelector("#btnRecargar");
-
-  function comprobarRespuesta() {
-    let objetoEnpantalla = Object.values(preguntasJson[contador]);
-    let respuestaCorrecta = objetoEnpantalla[2];
-    const inputs = document.querySelectorAll("#lista input");
-    //console.log(inputs)
-
-    let respuestaSeleccionada = null;
-    let spanRespuesta = null;
-
-    //recorremos los inputs para ver cual esta checkeado
-
-    for (const j of inputs) {
-      //comprobamos si esta seleccionado
-      if (j.checked) {
-        //el .parentNode es para coger al padre es decir al li
-        respuestaSeleccionada = j.parentNode;
-        console.log(respuestaSeleccionada);
-
-        //Cogemos el span del li seleccionado
-        spanRespuesta = respuestaSeleccionada.querySelector("span");
-        //En el siguiente console log tenemos la respuesta correcta
-        console.log(spanRespuesta.innerText);
-
-        if (spanRespuesta.innerText === respuestaCorrecta) {
-          console.log("acertaste");
-          aciertos++;
-          console.log(aciertos);
-        }
+    function recorrerLi(opciones) {
+      const ul = document.getElementById("lista").getElementsByTagName("li");
+      for (let i = 0; i < opciones.length; i++) {
+        const li = ul[i];
+        const span =
+          li.querySelector(
+            "span"
+          ); /* Se ingresa la respuesta en SPAN para no sobreescribir el INPUT RADIO */
+        span.textContent = opciones[i];
       }
     }
 
-    //esta es la respuesta correcta 
-    console.log(objetoEnpantalla[2]);
-  }
-  function guardarResultados(puntos) {
-    const nombre = prompt("Introduce tu nombre de usuario si quieres guardar la partida");
-    if (!nombre) return;
-    const ranking = JSON.parse(localStorage.getItem("ranking")) || [];
-    ranking.push({ nombre, puntos });
-    ranking.sort((a, b) => b.puntos - a.puntos); //esta funcion flecha lo que hace es guardar los puntos
-    localStorage.setItem("ranking", JSON.stringify(ranking.slice(0, 10))); //esta funcion nos guarda el top 10
-    console.log(ranking);
-  }
-  function mostrarResultado() {
-    //console.log("entro");
+    function hacerPregunta() {
+      let objetoEnpantalla = Object.values(preguntasJson[contador]);
+      //console.log(objetoEnpantalla[0]);
+      h2.innerHTML = `${objetoEnpantalla[0]}`;
 
-    const element = document.querySelector("#lista");
-    element.remove();
-    botonEnviar.remove();
-
-    guardarResultados(aciertos);
-
-    let objetoEnpantalla = Object.values(preguntasJson[contador]);
-    h2.innerHTML = `has acertado ${aciertos} de ${numeroPreguntas}`;
-
-
-    location.replace("final.html");
-  }
-
-
-  function recargarPagina() {
-    if (window.confirm("Quieres recargar la pagina?")) {
-      location.reload();
+      //console.log(objetoEnpantalla[1]);
+      let opciones = objetoEnpantalla[1];
+      recorrerLi(opciones);
     }
-  }
 
-  hacerPregunta();
+    const botonEnviar = document.querySelector("#btnEnviar");
+    const botonRecargar = document.querySelector("#btnRecargar");
 
-  botonEnviar.onclick = function () {
-    comprobarRespuesta();
+    function comprobarRespuesta() {
+      let objetoEnpantalla = Object.values(preguntasJson[contador]);
+      let respuestaCorrecta = objetoEnpantalla[2];
+      const inputs = document.querySelectorAll("#lista input");
+      //console.log(inputs)
 
-    //console.log("hiciste clik");
+      let respuestaSeleccionada = null;
+      let spanRespuesta = null;
 
-    contador++;
+      //recorremos los inputs para ver cual esta checkeado
+
+      for (const j of inputs) {
+        //comprobamos si esta seleccionado
+        if (j.checked) {
+          //el .parentNode es para coger al padre es decir al li
+          respuestaSeleccionada = j.parentNode;
+          console.log(respuestaSeleccionada);
+
+          //Cogemos el span del li seleccionado
+          spanRespuesta = respuestaSeleccionada.querySelector("span");
+          //En el siguiente console log tenemos la respuesta correcta
+          console.log(spanRespuesta.innerText);
+
+          if (spanRespuesta.innerText === respuestaCorrecta) {
+            console.log("acertaste");
+            aciertos++;
+            console.log(aciertos);
+          }
+        }
+      }
+
+      //esta es la respuesta correcta
+      /* buscarCorrecta(inputs, respuestaCorrecta); */
+      console.log(objetoEnpantalla[2]);
+    }
+
+    /* function buscarCorrecta(opciones, respuestaCorrecta) {
+      const ul = document.getElementById("lista").getElementsByTagName("li");
+      for (let i = 0; i < opciones.length; i++) {
+        const li = ul[i];
+        const span = li.querySelector("span");
+        let spanActual = span.textContent;
+        console.log(
+          spanActual
+        ); 
+        /* if (respuestaCorrecta === spanActual) {
+          console.log(spanActual + "este");
+          spanActual.style.textDecoration = "underline";
+        }
+      }
+    } */
+
+    function guardarResultados(puntos) {
+      const nombre = prompt(
+        "Introduce tu nombre de usuario si quieres guardar la partida"
+      );
+      if (!nombre) return;
+      const ranking = JSON.parse(localStorage.getItem("ranking")) || [];
+      ranking.push({ nombre, puntos });
+      ranking.sort((a, b) => b.puntos - a.puntos); //esta funcion flecha lo que hace es guardar los puntos
+      localStorage.setItem("ranking", JSON.stringify(ranking.slice(0, 10))); //esta funcion nos guarda el top 10
+      console.log(ranking);
+    }
+    function mostrarResultado() {
+      //console.log("entro");
+
+      /* const element = document.querySelector("#lista");
+      element.remove();
+      botonEnviar.remove();
+
+      guardarResultados(aciertos);
+
+      let objetoEnpantalla = Object.values(preguntasJson[contador]);
+      h2.innerHTML = `has acertado ${aciertos} de ${numeroPreguntas}`;
+ */
+      location.replace("final.html");
+    }
+
+    function recargarPagina() {
+      if (window.confirm("Quieres recargar la pagina?")) {
+        location.reload();
+      }
+    }
+
+    if (numeroPreguntas === 50) {
+      numeroPreguntas = numeroPreguntas - 1;
+    }
     hacerPregunta();
-    if (contador >= numeroPreguntas) {
-      mostrarResultado();
-    }
-  };
-  botonRecargar.onclick = function () {
-    recargarPagina();
-  }
 
-}).catch(function (e) {
-  console.error("ha surgido el siguiente error: " + e)
-});
+    botonEnviar.onclick = function () {
+      comprobarRespuesta();
 
+      //console.log("hiciste clik");
 
+      contador++;
 
-
-
-
-
+      if (contador > numeroPreguntas) {
+        mostrarResultado();
+      } else {
+        hacerPregunta();
+      }
+    };
+    botonRecargar.onclick = function () {
+      recargarPagina();
+    };
+  })
+  .catch(function (e) {
+    console.error("ha surgido el siguiente error: " + e);
+  });
